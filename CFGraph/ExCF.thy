2301396a338564bb05a8a3e88c1ef99609723b2c
Joachim Breitner <mail@joachim-breitner.de>
2010-08-31 14:52:09 +0200

    Implement ExCF (non-tail-recursive for now)

--- CFGraph/ExCF.thy
+++ CFGraph/ExCF.thy
@@ -14,7 +14,7 @@ datatype d = DI int
 types venv = "var \<times> contour \<rightharpoonup> d"
 
 types ccache = "label \<times> benv \<rightharpoonup> d"
-      ans = unit
+      ans = ccache
 
 fun evalV :: "val \<Rightarrow> benv \<Rightarrow> venv \<Rightarrow> d"
   where "evalV (C _ i) \<beta> ve = DI i"
@@ -24,13 +24,43 @@ fun evalV :: "val \<Rightarrow> benv \<Rightarrow> venv \<Rightarrow> d"
               Some l \<Rightarrow> (case ve (var,l) of Some d \<Rightarrow> d))"
   |     "evalV (L lam) \<beta> ve = DC (lam, \<beta>)"
 
-fun evalF :: "d \<Rightarrow> (d list) \<Rightarrow> venv \<Rightarrow> contour \<Rightarrow> ans"
-and evalC :: "call \<Rightarrow> benv \<Rightarrow> venv \<Rightarrow> contour \<Rightarrow> ans"
-  where "evalF (DC (Lambda lab vs c, \<beta>)) as ve b = ()"
-  |     "evalF (DP (Plus c)) [DI a1, DI a2, cont] ve b = ()"
-  |     "evalF (DP (If ct cf)) [DI v, contt, contf] ve b = ()"
-  |     "evalF Stop [DI i] _ _ = ()"
-  |     "evalC (App lab f vs) \<beta> ve b = ()"
-  |     "evalC (Let lab ls c') \<beta> ve b = ()"
+function (sequential,domintros)
+         evalF :: "d \<Rightarrow> (d list) \<Rightarrow> venv \<Rightarrow> contour \<Rightarrow> ans"
+     and evalC :: "call \<Rightarrow> benv \<Rightarrow> venv \<Rightarrow> contour \<Rightarrow> ans"
+  where "evalF (DC (Lambda lab vs c, \<beta>)) as ve b
+          = (case length vs = length as of
+              True \<Rightarrow> let \<beta>' = \<beta> (lab \<mapsto> b);
+                          ve' = map_upds ve (map (\<lambda>v.(v,b)) vs) as
+                      in evalC c \<beta>' ve' b
+            )"
+  |     "evalF (DP (Plus c)) as ve b
+          = (case as of  [DI a1, DI a2, cont] \<Rightarrow>
+                   let b' = Suc b;
+                       \<beta>  = [c \<mapsto> b]
+                   in evalF cont [DI (a1 + a2)] ve b' ((c, \<beta>) \<mapsto> cont))"
+  |     "evalF (DP (If ct cf)) [DI v, contt, contf] ve b
+          = (      if v \<noteq> 0
+                   then let b' = Suc b;
+                            \<beta> = [ct \<mapsto> b]
+                        in evalF contt [] ve b
+                   else let b' = Suc b;
+                            \<beta> = [cf \<mapsto> b]
+                        in evalF contf [] ve b')"
+  |     "evalF Stop as _ _
+          = (case as of [DI i] \<Rightarrow> empty)"
+
+  |     "evalC (App lab f vs) \<beta> ve b
+          = (let f' = evalV f \<beta> ve;
+                 as = map (\<lambda>v. evalV v \<beta> ve) vs;
+                 b' = Suc b
+             in evalF f' as ve b' ((lab,\<beta>) \<mapsto> f'))"
+  |     "evalC (Let lab ls c') \<beta> ve b
+          = (let b' = Suc b;
+                 \<beta>' = \<beta> (lab \<mapsto> b');
+                 ve' = ve ++ map_of (map (\<lambda>(v,l). ((v,b'), evalV (L l) \<beta>' ve)) ls)
+            in evalC c' \<beta>' ve' b')"
+  apply pat_completeness
+  apply auto
+done
 
 end
