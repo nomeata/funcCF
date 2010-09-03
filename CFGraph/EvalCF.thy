207950235cc30b6cd380a01a34151e738897f9b6
Joachim Breitner <mail@joachim-breitner.de>
2010-09-03 21:43:39 +0200

    Define Eval theory using HOLCF

---
+++ CFGraph/EvalCF.thy
@@ -0,0 +1,145 @@
+theory ExCF
+  imports CPSUtils HOLCF HOLCFUtils HOLCFList HOLCFOption CPSScheme
+begin
+
+types contour = nat
+      benv = "label \<rightharpoonup> contour"
+      closure = "lambda \<times> benv"
+
+datatype d = DI int
+           | DC closure
+           | DP prim
+           | Stop
+
+instantiation d :: discrete_cpo begin
+definition  [simp]: "(x::d) \<sqsubseteq> y \<longleftrightarrow> x = y"
+instance by default simp
+end
+
+instantiation call :: discrete_cpo begin
+definition  [simp]: "(x::call) \<sqsubseteq> y \<longleftrightarrow> x = y"
+instance by default simp
+end
+
+types venv = "var \<times> contour \<rightharpoonup> d"
+
+fun evalV :: "val \<Rightarrow> benv \<Rightarrow> venv \<Rightarrow> d"
+  where "evalV (C _ i) \<beta> ve = DI i"
+  |     "evalV (P prim) \<beta> ve = DP prim"
+  |     "evalV (R _ var) \<beta> ve =
+           (case \<beta> (binder var) of
+              Some l \<Rightarrow> (case ve (var,l) of Some d \<Rightarrow> d))"
+  |     "evalV (L lam) \<beta> ve = DC (lam, \<beta>)"
+
+
+types ccache = "((label \<times> benv) \<times> d) set"
+      ans = "int option"
+
+(*
+definition covers :: "prog \<Rightarrow> benv \<Rightarrow>venv \<Rightarrow> bool" where
+     "covers prog \<beta> ve = (\<forall> l\<in>dom \<beta>. \<forall> v \<in> vars prog. (binder v = l \<longrightarrow> (case \<beta> l of Some cnt \<Rightarrow> (v,cnt) \<in> dom ve)))"
+
+definition ve_consistent :: "prog \<Rightarrow> call \<Rightarrow> benv \<Rightarrow> venv \<Rightarrow> bool" where
+     "ve_consistent prog c \<beta> ve =
+        ((\<forall> ctx \<in> subterms (Inl prog). (Inr c \<in> subterms ctx \<longrightarrow> label ctx \<in> dom \<beta>))
+         \<and> covers prog \<beta> ve)"
+
+definition closure_consistent :: "prog \<Rightarrow> closure \<Rightarrow> venv \<Rightarrow> bool" where
+     "closure_consistent prog cls ve =
+        ((\<forall> ctx \<in> subterms (Inl prog). (Inl (fst cls) \<in> subterms ctx \<longrightarrow> label ctx \<in> dom (snd cls)))
+         \<and> covers prog (snd cls) ve)"
+
+definition cstate_ok :: "cstate \<Rightarrow> bool" where
+     "cstate_ok s = (case s of (c,\<beta>,ve,_) \<Rightarrow> ve_consistent c \<beta> ve)"
+*)
+
+types fstate = "(d \<times> d list \<times> venv \<times> contour)"
+      cstate = "(call \<times> benv \<times> venv \<times> contour)"
+
+lemma cont2cont_lambda_case [simp, cont2cont]:
+  assumes "\<And>a b c. cont (\<lambda>x. f x a b c)"
+  shows "cont (\<lambda>x. lambda_case (f x) l)"
+using assms
+by (cases l) auto
+
+lemma cont2cont_d_case [simp, cont2cont]:
+  assumes "\<And>y. cont (\<lambda>x. f1 x y)"
+     and  "\<And>y. cont (\<lambda>x. f2 x y)"
+     and  "\<And>y. cont (\<lambda>x. f3 x y)"
+    and   "cont (\<lambda>x. f4 x)"
+  shows "cont (\<lambda>x. d_case (f1 x) (f2 x) (f3 x) (f4 x) d)"
+using assms
+by (cases d) auto
+
+value call_case
+lemma cont2cont_call_case [simp, cont2cont]:
+  assumes "\<And>a b c. cont (\<lambda>x. f1 x a b c)"
+     and  "\<And>a b c. cont (\<lambda>x. f2 x a b c)"
+  shows "cont (\<lambda>x. call_case (f1 x) (f2 x) c)"
+using assms
+by (cases c) auto
+
+lemma cont2cont_prim_case [simp, cont2cont]:
+  assumes "\<And>y. cont (\<lambda>x. f1 x y)"
+     and  "\<And>y z. cont (\<lambda>x. f2 x y z)"
+  shows "cont (\<lambda>x. prim_case (f1 x) (f2 x) p)"
+using assms
+by (cases p) auto
+
+
+fixrec   evalF :: "fstate discr \<rightarrow> ans"
+     and evalC :: "cstate discr \<rightarrow> ans"
+  where "evalF\<cdot>fstate = (case undiscr fstate of
+             (DC (Lambda lab vs c, \<beta>), as, ve, b) \<Rightarrow>
+               (if length vs = length as
+                then let \<beta>' = \<beta> (lab \<mapsto> b);
+                         ve' = map_upds ve (map (\<lambda>v.(v,b)) vs) as
+                     in evalC\<cdot>(Discr (c,\<beta>',ve',b))
+                else \<bottom>)
+            | (DP (Plus c),[DI a1, DI a2, cnt],ve,b) \<Rightarrow>
+                     let b' = Suc b;
+                         \<beta>  = [c \<mapsto> b]
+                     in evalF\<cdot>(Discr (cnt,[DI (a1 + a2)],ve,b'))
+            | (DP (prim.If ct cf),[DI v, contt, contf],ve,b) \<Rightarrow>
+                  (if v \<noteq> 0
+                   then let b' = Suc b;
+                            \<beta> = [ct \<mapsto> b]
+                        in evalF\<cdot>(Discr (contt,[],ve,b'))
+                   else let b' = Suc b;
+                            \<beta> = [cf \<mapsto> b]
+                        in evalF\<cdot>(Discr (contf,[],ve,b')))
+            | (Stop,[DI i],_,_) \<Rightarrow> Some i
+        )"
+      | "evalC\<cdot>cstate = (case undiscr cstate of
+             (App lab f vs,\<beta>,ve,b) \<Rightarrow>
+                 let f' = evalV f \<beta> ve;
+                     as = map (\<lambda>v. evalV v \<beta> ve) vs;
+                     b' = Suc b
+                  in evalF\<cdot>(Discr (f',as,ve,b'))
+            | (Let lab ls c',\<beta>,ve,b) \<Rightarrow>
+                 let b' = Suc b;
+                     \<beta>' = \<beta> (lab \<mapsto> b');
+                    ve' = ve ++ map_of (map (\<lambda>(v,l). ((v,b'), evalV (L l) \<beta>' ve)) ls)
+                 in evalC\<cdot>(Discr (c',\<beta>',ve',b'))
+        )"
+
+print_theorems
+
+
+definition evalCPS :: "prog \<Rightarrow> ans"
+  where "evalCPS l = (let ve = empty;
+                          \<beta> = empty;
+                          f = evalV (L l) \<beta> ve
+                      in  evalF\<cdot>(Discr (f,[Stop],ve,0)))"
+
+(*
+lemma correct_ex1: "evalCPS ex1 \<sqsubseteq> Some 0"
+unfolding evalCPS_def
+proof(induct rule:evalF_evalC.induct)
+print_cases
+  case 2 show ?case by simp
+  next
+*)
+
+
+end
